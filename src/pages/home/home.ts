8b20d086dd01382d0bf05460e3605cd140796169
Nitesh.ldd <nitesh.ldd@gmail.com>
2018-07-08 09:02:17 +0530

    Addesss function to fab button to send data to server

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController , AlertController} from 'ionic-angular';
-
+import { Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+  url: string;
+  headers: Headers;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: Http) {
+    this.headers = new Headers();
+    this.headers.append("X-Parse-Application-Id","AppId1");
+    this.headers.append("X-Parse-REST-API-Key","restAPIKey");
+    this.headers.append("X-Parse-Revocable-Session","1");
 
   }
 
@@ -32,6 +40,32 @@ export class HomePage {
           text: "Save",
           handler: data => {
             //post the information to parse server
+            this.url ="http://ec2-34-219-71-164.us-west-2.compute.amazonaws.com:9000/app1/classes/friendslist"
+            this.http.post(this.url, {name: data.name, email: data.email, number: data.number, image: "https://www.uruvelaa.com/uruvela_dev/Creative%20Tim%20-%20Fully%20coded%20UI%20Tools%20to%20create%20web%20apps_files/paperkit.jpg"},{headers: this.headers}).map(res => res.json()).subscribe(res => {
+              console.log(res);
+              this.alertCtrl.create({
+                title: "Success",
+                message: "Friend Information Saved Successfully",
+                buttons : [{
+                  text: "OK",
+                  handler: ()=>{
+
+                  }
+                }]
+              }).present();
+            },err =>{
+              console.log(err);
+              this.alertCtrl.create(
+                {
+                  title : "Error",
+                  message : err.text(),
+                  buttons: [{
+                    text: "OK"
+                  }]
+                }
+              ).present();
+            })
+            
           }
         }
       ]   
